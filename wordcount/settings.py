28821ca7b0599992ae96e876e68e592f02c22cc5
Robert Mwanzia <rmwanzia@gmail.com>
2019-01-18 15:24:05 +0300

    Added static image functionality

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Folder for static media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/images/'
--- wordcount/urls.py
+++ wordcount/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', views.homepage, name='home'),
     path('count/', views.count, name='count'),
     path('about', views.about, name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
